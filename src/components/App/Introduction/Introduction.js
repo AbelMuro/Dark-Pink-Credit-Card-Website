c21224f1d32c190507a65392a3e5e516247d5d58
abelmuro <abelmuro93@gmail.com>
2022-09-12 11:04:59 -0700

    halfway done with first animation

--- src/components/App/Introduction/Introduction.js
+++ src/components/App/Introduction/Introduction.js
@@ -15,20 +15,39 @@ function Introduction() {
 
 
     useEffect(() => {
-        gsap.timeline()
+        gsap.timeline({scrollTrigger: {
+            trigger: ".introOne",
+            start: "10% 10%",
+            end: "20% 20%",
+            scrub: 1,
+            markers: true
+        }})
             .to(nestedElement("." + styles.creditCard), {
+                position: "fixed",
+                zIndex: 2,
                 transform: "scale(1.2, 1.2)",
+                x: -150,
                 duration: 0.6
-            })
-    });
+            }, 0)
+            .to(nestedElement("." + styles.creditCard), {
+                rotation: 360,
+                duration: 0.6
+            }, 0)
+    },[]);
 
     return(
-        <main className={styles.intro} ref={creditCardAnimation}> 
-           <img src={creditCard} className={styles.creditCard}/>
-           <p className={styles.benefitOne}> 5% cashback on any purchase</p>
-           <p className={styles.benefitTwo}> 7% cashback on restaurants, gas stations, and grocery stores</p>
-           <p className={styles.benefitThree}> 10% discount on purchasing dark pink stuff!</p>
-        </main>
+        <>
+            <main className={styles.introOne} ref={creditCardAnimation}> 
+                <img src={creditCard} className={styles.creditCard}/>
+               { /*<p className={styles.benefitOne}> 5% cashback on any purchase</p> */ }
+               { /* <p className={styles.benefitTwo}> 7% cashback on restaurants, gas stations, and grocery stores</p> */ }
+                { /* <p className={styles.benefitThree}> 10% discount on purchasing dark pink stuff!</p> */}
+            </main>
+            <section className={styles.introTwo}>
+
+            </section>        
+        </>
+
     )
 
 }
